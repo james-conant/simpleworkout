49fd81344cfa762c7d7d551e3c5e854f7ccd8c82
James Contant <>
2024-01-13 13:21:44 -0600

    initial commit.

---
+++ src/main/java/com/simpleworkout/simpleworkout/SimpleworkoutApplication.java
@@ -0,0 +1,13 @@
+package com.simpleworkout.simpleworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleworkoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleworkoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpleworkout/simpleworkout/SimpleworkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleworkout.simpleworkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleworkoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
