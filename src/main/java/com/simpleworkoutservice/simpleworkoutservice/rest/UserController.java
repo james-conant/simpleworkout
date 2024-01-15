7c1b0d2446ca589ba6180a65d960cb5519d413e1
James Contant <>
2024-01-14 22:55:48 -0600

    Adds user controller and service

---
+++ src/main/java/com/simpleworkoutservice/simpleworkoutservice/rest/UserController.java
@@ -0,0 +1,72 @@
+package com.simpleworkoutservice.simpleworkoutservice.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.simpleworkoutservice.simpleworkoutservice.entity.User;
+import com.simpleworkoutservice.simpleworkoutservice.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable("id") int id) {
+
+        User user = userService.findById(id);
+
+        if (user == null) {
+            throw new RuntimeException("User id not found - " + id);
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User user) {
+
+        user.setId(0);
+
+        User dbUser = userService.save(user);
+        return dbUser;
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) {
+
+        User dbUser = userService.save(user);
+
+        return dbUser;
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable int userId) {
+
+        User dbUser = userService.findById(userId);
+
+        // throw exception if null
+
+        if (dbUser == null) {
+            throw new RuntimeException("User id not found - " + userId);
+        }
+
+        userService.deleteById(userId);
+
+        return "Deleted user id - " + userId;
+    }
+
+}
